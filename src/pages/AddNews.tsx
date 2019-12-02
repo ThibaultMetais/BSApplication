e356a8f1e093197c2d24246caa757a7aa38fb72e
Thibault Metais <thibault.metais31@gmail.com>
2019-12-02 11:30:37 -0300

    Blank post cannot be submitted

--- src/pages/AddNews.tsx
+++ src/pages/AddNews.tsx
@@ -8,7 +8,8 @@ import { IonHeader,
   IonButton,
   IonBackButton,
   IonLabel,
-  IonTextarea
+  IonTextarea,
+  IonAlert
 } from '@ionic/react';
 import axios from 'axios';
 
@@ -17,6 +18,7 @@ const AddNewsPage: React.FC = () => {
   const [content, setContent] = useState();
   const [message, setMessage] = useState();
   const [colormessage, setColormessage] = useState('#2ecc71');
+  const [showAlert1, setShowAlert1] = useState(false);
 
   const section = {
     'font-weight': 'bold' as 'bold'
@@ -36,19 +38,24 @@ const AddNewsPage: React.FC = () => {
   }
 
   function postNews() {
-    axios.post('https://app-ae25ef7f-eff0-469a-8f73-ef2bbd4b6965.cleverapps.io/api/news/', {
-      content: content,
-    })
-    .then((response) => {
-      console.log(response);
-      setContent(null)
-      setColormessage('#2ecc71')
-      setMessage("New post submitted !")
-    }, (error) => {
-      console.log(error);
-      setColormessage('#e74c3c')
-      setMessage(error)
-    });
+    if ((content != "") && (content != null)) {
+      axios.post('https://app-ae25ef7f-eff0-469a-8f73-ef2bbd4b6965.cleverapps.io/api/news/', {
+        content: content,
+      })
+      .then((response) => {
+        console.log(response);
+        setContent(null)
+        setColormessage('#2ecc71')
+        setMessage("New post submitted !")
+      }, (error) => {
+        console.log(error);
+        setColormessage('#e74c3c')
+        setMessage(error)
+      });
+    }
+    else {
+      setShowAlert1(true)
+    }
   }
 
   return (
@@ -68,6 +75,14 @@ const AddNewsPage: React.FC = () => {
           <IonButton expand="block" onClick={() => postNews()}>Submit</IonButton>
           <p style={messagestyle}>{message}</p>
         </div>
+        <IonAlert
+          isOpen={showAlert1}
+          onDidDismiss={() => setShowAlert1(false)}
+          header={'Error'}
+          subHeader={'Post cannot be submitted'}
+          message={'Post cannot be blank.'}
+          buttons={['Ok']}
+        />
       </IonContent>
     </IonPage>
   );
